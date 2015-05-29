15839a65deb1761c4f9ef947252afce95852f1f1
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2015-05-29 15:03:51 -0400

    Removed unused function

--- apps/render/index.js
+++ apps/render/index.js
@@ -10,15 +10,3 @@ service.get('rooms/' + roomId)
   .then(function (model) {
     app.room = model.get('_page.room');
   });
-
-function getQueryVariable(variable) {
-    var query = window.location.search.substring(1);
-    var vars = query.split('&');
-    for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split('=');
-        if (decodeURIComponent(pair[0]) == variable) {
-            return decodeURIComponent(pair[1]);
-        }
-    }
-    console.log('Query variable %s not found', variable);
-}
