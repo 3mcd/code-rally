bd114b80eb76bd39c877936f389ad8b3292f7942
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2015-06-14 00:35:34 -0400

    Added CSS spinner component

--- apps/client/components/cr-render.vue
+++ apps/client/components/cr-render.vue
@@ -3,6 +3,7 @@
     flex 1
     display flex
     flex-direction column
+    position relative
     
   .cr-Render-frame
     border 0
@@ -10,11 +11,28 @@
 </style>
 
 <template>
+    <cr-spinner v-if="loading"></cr-spinner>
     <iframe v-el="render" src="/render/{{room.name}}?ts={{room.reload ? room.ts : ''}}" allowfullscreen="true" sandbox="allow-scripts allow-pointer-lock allow-same-origin allow-popups allow-forms" allowtransparency="true" class="cr-Render-frame"></iframe>
 </template>
 
 <script>
   module.exports = {
-    props: ['room']
+    data: function () {
+      return {
+        loading: true
+      }
+    },
+    props: ['room'],
+    ready: function () {
+      var _this = this;
+      this.$$.render.onload = function () {
+        _this.loading = false;
+      }
+    },
+    watch: {
+      'room.ts': function () {
+        this.loading = true;
+      }
+    }
   };
 </script>
--- apps/client/components/cr-room.vue
+++ apps/client/components/cr-room.vue
@@ -33,28 +33,31 @@
 </style>
 
 <template>
-  <v-panel-bar size="auto">
-    <cr-tabs tabs="{{tabs}}" v-ref="tabs"></cr-tabs>
-  </v-panel-bar>
-  <v-panel grow="1" wrap="wrap" align-items="stretch">
-    <v-panel grow="2" basis="300px" direction="column" class="cr-Room-editors">
-      <template v-repeat="editor: room.editors">
-        <cr-editor editor="{{editor}}" room="{{room}}" meta="{{meta}}" v-if="editor == meta.active"></cr-editor>
-      </template>
-    </v-panel>
-    <v-panel class="cr-Room-render" v-class="is-active: meta.render.visible" direction="column">
-      <v-panel-bar class="cr-Room-renderBar" size="2em">
-        <v-panel grow="2">
-          <h2 v-show="meta.render.visible" class="cr-Room-title">rooms/{{room.name}}</h2>
+  <cr-spinner v-if="!meta.loaded"></cr-spinner>
+  <v-panel v-if="meta.loaded" direction="column">
+    <v-panel-bar size="auto">
+      <cr-tabs tabs="{{tabs}}" v-ref="tabs"></cr-tabs>
+    </v-panel-bar>
+    <v-panel grow="1" wrap="wrap" align-items="stretch">
+      <v-panel grow="2" basis="300px" direction="column" class="cr-Room-editors">
+        <template v-repeat="editor: room.editors">
+          <cr-editor editor="{{editor}}" room="{{room}}" meta="{{meta}}" v-if="editor == meta.active"></cr-editor>
+        </template>
+      </v-panel>
+      <v-panel class="cr-Room-render" v-class="is-active: meta.render.visible" direction="column">
+        <v-panel-bar class="cr-Room-renderBar" size="2em">
+          <v-panel grow="2">
+            <h2 v-show="meta.render.visible" class="cr-Room-title">rooms/{{room.name}}</h2>
+          </v-panel>
+          <v-panel grow="0">
+            <button v-on="click: meta.render.visible = !meta.render.visible">
+              <cr-icon type="{{meta.render.visible ? 'left4' : 'right4'}}"></cr-icon>
+            </button>
+          </v-panel>
+        </v-panel-bar>
+        <v-panel grow="1">
+          <cr-render room="{{room}}"></cr-render>
         </v-panel>
-        <v-panel grow="0">
-          <button v-on="click: meta.render.visible = !meta.render.visible">
-            <cr-icon type="{{meta.render.visible ? 'left4' : 'right4'}}"></cr-icon>
-          </button>
-        </v-panel>
-      </v-panel-bar>
-      <v-panel grow="1">
-        <cr-render room="{{room}}"></cr-render>
       </v-panel>
     </v-panel>
   </v-panel>
@@ -90,7 +93,8 @@
       'cr-editor': require('./cr-editor.vue'),
       'cr-render': require('./cr-render.vue'),
       'cr-tabs': require('./cr-tabs.vue'),
-      'cr-icon': require('./cr-icon.vue')
+      'cr-icon': require('./cr-icon.vue'),
+      'cr-spinner': require('./cr-spinner.vue')
     },
     data: function () {
       return {
